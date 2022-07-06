73c9bb88ae43497883f4b8cd54099306e78029c1
Carlos Blanco <carlosblanco@me.com>
2022-07-06 15:53:01 +0200

    Api call

--- src/lib/Weather.svelte
+++ src/lib/Weather.svelte
@@ -1,9 +1,32 @@
 <script>
 
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f8d9521e48msh7f5069aa2cea2a8p10d432jsnd92a13465de7',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	}
+};
+
+const weatherPromise = fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=Madrid', options)
+	.then(response => response.json())
+	.then(response => {
+    console.log(response);
+    const { location, current } = response;
+    const { country } = location;
+    const { condition } = current;
+    return {
+      condition,
+      country
+    }
+  })
+
 </script>
 
-<p>Test</p>
-
+{#await weatherPromise then weather}
+ <h1>{ weather.country }</h1>
+{/await}
 <style>
  
 </style>
